463a43df0b0f5ab6ab5b873ed50c459e035bad30
Deston <destonchung@gmail.com>
2024-10-26 00:00:47 +0800

    Update loading in prediction.py to use cpu

--- model/prediction.py
+++ model/prediction.py
@@ -366,7 +366,7 @@ def predict_with_models(df):
 
 
     model = MultiLabelClassifier(num_labels=num_aspects).to(device)
-    model.load_state_dict(torch.load(ASPECT_MODEL_DISTIL))
+    model.load_state_dict(torch.load(ASPECT_MODEL_DISTIL, map_location=torch.device(device)))
 
     result = predict_distill_aspect_scores(model, tokenizer, result)
     result['distil_aspect_labels'] = result.distil_aspect_scores.apply(lambda x: np.where(np.array(x) >= 0.35, 1.0, 0.0)) # Returns list of length 13, if all 0 then its others.
@@ -407,7 +407,7 @@ def predict_with_models(df):
     tokenizer = AutoTokenizer.from_pretrained(DISTILBERT_BASE_CASED)
     # torch.save(model, 'sentiment_model_val_acc_6162_lr4.5e-5_wtdecay_1e-4_epochs4_256_256_256_256_warmup_and_reducelr.pth')
         
-    model = torch.load(SENTIMENT_MODEL_DISTIL)
+    model = torch.load(SENTIMENT_MODEL_DISTIL, map_location=torch.device(device))
     model.to(device)
 
     result['distil_sentiment_prediction_and_confidence_score'] = result.apply(lambda x: predict_sentiment_distil(x, tokenizer, model, return_both=True), axis=1)
