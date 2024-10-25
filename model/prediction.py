3336afe934bc2b43d7536ce9e523e27adc8d3c2d
Deston <destonchung@gmail.com>
2024-10-26 00:06:40 +0800

    Fix bug ABSM loading bug by using load state dict

--- model/prediction.py
+++ model/prediction.py
@@ -408,7 +408,7 @@ def predict_with_models(df):
     # torch.save(model, 'sentiment_model_val_acc_6162_lr4.5e-5_wtdecay_1e-4_epochs4_256_256_256_256_warmup_and_reducelr.pth')
         
     model = AspectBasedSentimentModel(DISTILBERT_BASE_CASED)
-    model.load(SENTIMENT_MODEL_DISTIL, map_location=torch.device(device))
+    model.load_state_dict(torch.load(SENTIMENT_MODEL_DISTIL, map_location=torch.device(device)))
     model.to(device)
 
     result['distil_sentiment_prediction_and_confidence_score'] = result.apply(lambda x: predict_sentiment_distil(x, tokenizer, model, return_both=True), axis=1)
